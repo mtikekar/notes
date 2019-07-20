9489562e227566f58902e2b7a0de81ffe6a6fe93
Mehul Tikekar <mehultikekar@gmail.com>
2019-07-19 23:55:14 -0400

    minor changes to tf conda parsing script

--- tfversions.py
+++ tfversions.py
@@ -1,28 +1,22 @@
 import subprocess as sp
 import json
 
+def is_gpu(pkg):
+    return any(dep.startswith('cudatoolkit') for dep in pkg['depends'])
+
+def is_revoked(pkg):
+    return any(dep == 'package_has_been_revoked' for dep in pkg['depends'])
+
 v = sp.check_output(['conda', 'info', '--json', 'tensorflow-base'])
 v = json.loads(v)
 v = v['tensorflow-base']
-
-def is_gpu(pkg):
-    for dep in pkg['depends']:
-        if dep.startswith('cudatoolkit'):
-            return True
-    return False
-
-def is_revoked(pkg):
-    for dep in pkg['depends']:
-        if dep == 'package_has_been_revoked':
-            return True
-    return False
 
 for pkg in v:
     if is_revoked(pkg) or not is_gpu(pkg):
         continue
 
-    print(pkg['version'], end=' ')
+    print(pkg['version'], end='\t')
     for dep in pkg['depends']:
         if dep.startswith('python') or dep.startswith('cudatoolkit'):
-            print(dep, end=' ')
+            print(dep, end='\t')
     print()
